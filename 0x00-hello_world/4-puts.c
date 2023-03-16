59177e0d70443709dcd89ae38b64a329549af6ee
Gus-Victrix <iamfrsncis@gmail.com>
2023-03-17 02:21:32 +0300

    FILE ADDITION IN: 0x00-hello_world
    The file 4-puts.c was added to the repository

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - It calls puts() to print to stdout
+ *
+ * Return: 0 after successful excecution because it's an int type function
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
