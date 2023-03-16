89fa97c4cc45719ebfc8e02ef7514e215fd51863
Gus-Victrix <iamfrsncis@gmail.com>
2023-03-17 02:08:38 +0300

    FILE ADDITION IN: 0x00-hello_world
    The file 5-printf.c was added to the repository

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - uses printf to print to stdout
+ *
+ * Return: 0 after successful excecution
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
