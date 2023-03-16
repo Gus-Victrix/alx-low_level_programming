88dde6cf2f6c4218c80ae78a41ee910b2a2cbec4
Gus-Victrix <iamfrsncis@gmail.com>
2023-03-17 00:26:28 +0300

    FILE MODIFICATION IN: 0x00-hello_world
    File 101-quote modified to remove extra functions... Now only write() is used and only ulibrary is used

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,27 +1,14 @@
 #include <unistd.h>
-#include <string.h>
 
 /**
  * main - It contains the program
- * @words[]: The string to be printed.
  *
  * Description: The function calls the write() function to write words in:
  *	1. standard error.
- *	2. standard output.
- * The function strlen() has been used to determine length of the string for
- * write().
- *
- * Return: 1 after successful execution
  */
 int main(void)
 {
-	char *words = "and that piece of art is useful\" - Dora Koprar, 2015-10-19\n";
-	char *message = "This is not easy... In fact, it's downright annoying:(
-				einigierjfieieedffqefqaefawefqaef";
 
-	write(1, words, strlen(words));
-	write(STDERR_FILENO, message, strlen(words));
-	perror("Error not found");
-
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen(words));
 	return (1);
 }
