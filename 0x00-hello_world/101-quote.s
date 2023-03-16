7d477c2e4ed92a4e33b442a9f23fc6fcfa5636bc
Gus-Victrix <iamfrsncis@gmail.com>
2023-03-17 01:30:04 +0300

    FILE MODIFICAITON IN: 0x00-hello_world
    The file 100-intel has been modified so that gcc uses masm of intel syntax

---
+++ 0x00-hello_world/101-quote.s
@@ -0,0 +1,49 @@
+	.file	"101-quote.c"
+	.intel_syntax noprefix
+	.text
+	.section	.rodata
+	.align 8
+.LC0:
+	.string	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	mov	edx, 59
+	lea	rax, .LC0[rip]
+	mov	rsi, rax
+	mov	edi, 2
+	call	write@PLT
+	mov	eax, 1
+	pop	rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 11.3.0-1ubuntu1~22.04) 11.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
