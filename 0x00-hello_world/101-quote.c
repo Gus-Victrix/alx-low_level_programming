3f3796cfea12d56e1d32450d5a052fda5b294462
Gus-Victrix <iamfrsncis@gmail.com>
2023-03-16 22:35:16 +0300

    MODIFICATION IN 0X00-hello_world
    The file: 101-quote.c has been modified to send output to stderror instead of stdout

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,15 @@
-/*
- * When compiled, the resulting executable of this file prints to stderror.
-*/
-
 #include <unistd.h>
 #include <string.h>
 
+/*
+ * When compiled, the resulting executable of this file prints to stderror.
+*/
 int main(void)
 {
 	char words[] = "and that piece of art is useful\" - Dora Koprar, 2015-10-19\n"
 		;
 
-	write(1, words, strlen(words));
+	write(2, words, strlen(words));
 
 	return (1);
 }
