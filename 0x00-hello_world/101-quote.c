8ac8310e152979bc23e723340c5ef4139881c95c
Gus-Victrix <iamfrsncis@gmail.com>
2023-03-16 23:04:18 +0300

    MODIFICATION IN: 0x00-hello_world
    The file 101-quote.c has been modified to comply with Betty standards

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,24 @@
 #include <unistd.h>
 #include <string.h>
 
-/*
- * When compiled, the resulting executable of this file prints to stderror.
-*/
+/**
+ * main - It contains the program
+ * @words[]: The string to be printed.
+ *
+ * Description: The function calls the write() function to write words in:
+ *	1. standard error.
+ *	2. standard outputo.
+ * The function strlen() has been used to determine length of the string for
+ * write().
+ * 
+ * Return: 1 after successful execution
+ */
 int main(void)
 {
 	char words[] = "and that piece of art is useful\" - Dora Koprar, 2015-10-19\n"
 		;
 
+	write(1, words, strlen(words));
 	write(2, words, strlen(words));
 
 	return (1);
