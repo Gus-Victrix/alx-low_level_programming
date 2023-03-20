f5d59c2249832e70d213b77cd9f63c54243e5cb4
Gus-Victrix <iamfrsncis@gmail.com>
2023-03-21 00:34:49 +0300

    FILE MODIFICATION IN: 0x01-variables_if_else_while
    The file 102-print_comb5.c has been modified so that the output eliminates all duplicated combinations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,24 +9,23 @@ int main(void)
 {
 	int a;
 	int b;
-	int c;
-	int d;
 
-	for (a = 48; a < 58; a++)
-		for (b = a; b < 58; b++)
-			for (c = b; c < 58; c++)
-				for (d = c; d < 58; ++d)
-				{
-					putchar((char) a);
-					putchar((char) b);
-					putchar((char) 32);
-					putchar((char) c);
-					putchar((char) d);
+	for (a = 0; a <= 99; a++)
+		for (b = a + 1; b <= 99; b++)
+		{
+			putchar(a / 10 + 48);
+			putchar(a % 10 + 48);
+			putchar(32);
+			putchar(b / 10 + 48);
+			putchar(b % 10 + 48);
 
-					if (a == 57)
-						break;
-					putchar((char) 44);
-					putchar((char) 32);
-				}
+			if ((a == 98) && (b == 99))
+			{
+				putchar('\n');
+				break;
+			}
+			putchar(44);
+			putchar(32);
+		}
 	return (0);
 }
