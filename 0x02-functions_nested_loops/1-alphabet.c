d23ba7b3e39113413e9682a8e12b93bd70c0b632
Gus-Victrix <iamfrsncis@gmail.com>
2023-03-22 02:09:06 +0300

    FILE ADDITION IN:0x02-functions_nested_loops
    The file 1-alphabet.c has been added into the repo

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lowercase followed by newline using _putchar.
+ */
+
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	puthar('\n');
+
+	return;
+}
