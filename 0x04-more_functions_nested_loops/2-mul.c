c26c07c9dac80e481f3c0dae7b4317183e493924
Gus-Victrix <iamfrsncis@gmail.com>
2023-03-24 00:25:11 +0300

    ADDED: 2-mul.c
    Contains definition for mul() declared in main.h

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: Second integer
+ * Return: product of inputs
+ */
+
+int mul(int a, int b)
+{
+	a *= b;
+
+	return (a);
+}
