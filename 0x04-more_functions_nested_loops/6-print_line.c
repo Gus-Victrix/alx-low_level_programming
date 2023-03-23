a9819ade0f5dbe293102936a7faf2a3b73637161
Gus-Victrix <iamfrsncis@gmail.com>
2023-03-24 01:04:22 +0300

    ADDED: 6-print_line.c for task 6
    Contains definition for print_line() declared in main.h

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_line - Uses '_' to create a line of input length
+ * @n: The length of the line to be printed
+ */
+
+void print_line(int n)
+{
+	while (i--)
+		_putchar('_');
+	_putchar(10);
+}
