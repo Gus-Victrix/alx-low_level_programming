fe235ea6cb9dd6852895dcc65673e5557e6bfc69
Gus-Victrix <iamfrsncis@gmail.com>
2023-03-27 23:31:21 +0300

    ADDED task 0: 0-reset_to_98.c
    The file contains the definition for the function reset_to_98() declared in main.h

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - turns the value at input address to 98
+ * @*n: The address of the value to be modified.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
