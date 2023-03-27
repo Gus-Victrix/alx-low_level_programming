f7d23e87e5fe8b28a7b55e5be1326ad349a99a86
Gus-Victrix <iamfrsncis@gmail.com>
2023-03-27 23:38:41 +0300

    Mod: task 0, file: 0-reset_to_98.c
    Redescribed parameter as n instead of *n

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - turns the value at input address to 98
- * @*n: The address of the value to be modified.
+ * @n: The address of the value to be modified.
  */
 
 void reset_to_98(int *n)
