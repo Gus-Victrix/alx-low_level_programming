e6872c3159f7866321722a6417d668865183089e
Gus-Victrix <iamfrsncis@gmail.com>
2023-03-28 00:26:33 +0300

    ADDED: task 4, file 4-print_rev.c
    Defines print_rev() declared in main.h

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints the reverse of a string.
+ * @s: Character string to be reversed.
+ */
+
+void print_rev(char *s)
+{
+	char *tmp = s;
+
+	while (*s != 0)
+		s++;
+	do {
+		_putchar(*s);
+		s--;
+	} while (s != tmp)
+	_putchar(10);
+}
