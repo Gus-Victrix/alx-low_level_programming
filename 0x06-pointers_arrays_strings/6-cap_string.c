059960e9917eef62ab4b70662916030c4ce61cd7
Gus-Victrix <iamfrsncis@gmail.com>
2023-03-29 22:52:07 +0300

    Some minor syntatical changes to the file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,11 +20,11 @@ char *cap_string(char *s)
 
 	while (*mark)
 	{
-		if (*(mark - 1) == 10 || *(mark - 1) == 9 || *(mark - 1) >= 32 && <= 34
-				|| *(mark - 1) == 44 || *(mark - 1) == 46 || *(mark - 1) == 63
-				|| *(mark - 1) == 58 || *(mark - 1) == 59 || *(mark - 1) == 40
-				|| *(mark - 1) == 41 || *(mark - 1) == 123 || *(mark - 1) == 125
-				)
+		if (*(mark - 1) == 10 || *(mark - 1) == 9 || *(mark - 1) >= 32 &&
+				*(mark - 1) <= 34 || *(mark - 1) == 44 || *(mark - 1) == 46 ||
+				*(mark - 1) == 63 || *(mark - 1) == 58 || *(mark - 1) == 59 ||
+				*(mark - 1) == 40 || *(mark - 1) == 41 || *(mark - 1) == 123 ||
+				*(mark - 1) == 125)
 			if (*mark >= 97 && *mark <= 122)
 				*mark -= 32;
 		mark++;
