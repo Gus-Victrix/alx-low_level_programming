0312afc54089d622fd626a9a2d9c4e5d4ab104e6
Gus-Victrix <iamfrsncis@gmail.com>
2023-03-29 12:15:09 +0300

    Added task 4, file 4-rev_array.c containing definition of function reverse_array() whose prototype is in main.h

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_array - Reverses the contents of an array of integers.
+ *
+ * @a: Pointer to array of integers.
+ * @n: Number of elements in the array.
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0, temp;
+
+	n--;
+
+	while ((a + n) - i > a + i)
+	{
+		temp = *((a + n) - i);
+		*((a + n) - i) = *(a + i);
+		*(a + i) = temp;
+		i++;
+	}
+}
