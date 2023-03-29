5b6853925d0d3cfbdea52d3d02ed5ffc66d18f6b
Gus-Victrix <iamfrsncis@gmail.com>
2023-03-29 13:32:02 +0300

    Partially done task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes the first character of each word in input string.
+ *
+ * @s: Input string.
+ *
+ * Return: Pointer to output string.
+ */
+
+char *cap_string(char *s)
+{
+	//Mark starting point of string
+	//Capitalize first letter
+
+	//move new position in string
+	//check if previous character is one considered a word separator
+	//if true, capitalize current letter
+	//Repeat until end of string is reached.
+	char *mark = s;
+
+	if (*mark >= 97 && *mark <= 122)
+	{
+		*mark -= 32;
+		mark++;
+	}
+
+	while (*mark)
+	{
+		if (*(mark - 1) == 10 || *(mark - 1) == 9 || *(mark -1) >= 32 && <= 34
+				|| *(mark - 1) == 44 || *(mark - 1) == 46 || *(mark - 1) == 63
+				|| *(mark - 1) == 58 || *(mark - 1) == 59 || *(mark - 1 ) == 40
+				|| *(mark - 1) == 41 || *(mark - 1) == 123 || *(mark - 1) == 125
+				)
+			if (*mark >= 97 && *mark <= 122)
+				*mark -= 32;
+		mark++;
+	}
+
+	return (s);
+}
