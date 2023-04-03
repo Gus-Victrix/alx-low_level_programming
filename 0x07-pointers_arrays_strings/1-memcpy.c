b30fbde107cd50fa807c0ac648b55c47873d7406
Gus-Victrix <iamfrsncis@gmail.com>
2023-04-03 14:50:12 +0300

    ADDED TASK 1
    File contains the definition of function _memcpy() that works like memcpy()

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copies n bytes from memory area src to memory area dest.
+ *
+ * @n: Number of bytes from src to be copied into dest.
+ * @dest: The pointer to the destination memory area.
+ * @src: The source memory area.
+ *
+ * Return: Pointer to dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	if (dest == 0)
+		return (dest);
+
+	while (i++ < n)
+	{
+		*(dest + i) = *(src + i);
+	}
+
+	return (dest);
+}
