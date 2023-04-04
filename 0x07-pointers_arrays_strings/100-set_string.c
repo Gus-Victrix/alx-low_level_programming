5d54c91659db928e280af68b3787ae989a0e75cb
Gus-Victrix <iamfrsncis@gmail.com>
2023-04-04 03:02:46 +0300

    Added description for parameter to in file

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -4,7 +4,7 @@
  * set_string - Sets the value of a pointer to a char.
  *
  * @s: Address of pointer to string.
- * @: Address of char string.
+ * @to: Address of char string.
  */
 
 void set_string(char **s, char *to)
