d58e095bf7edb886f287f71839fc434853c6cdd8
Gus-Victrix <iamfrsncis@gmail.com>
2023-04-04 03:01:19 +0300

    ADDED TASK 8
    The file defines set_string() which changes the pointer pointed to by a double pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - Sets the value of a pointer to a char.
+ *
+ * @s: Address of pointer to string.
+ * @: Address of char string.
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
