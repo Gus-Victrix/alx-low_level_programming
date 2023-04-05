2f3838eb7b88b8bac24da8c36b7f7e91e3e1e1b2
Gus-Victrix <iamfrsncis@gmail.com>
2023-04-06 01:00:56 +0300

    ADDED TASK 8
    File defines wildcmp() which compares two strings allowing for wildcards in the form of *

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * wildcmp - Compares two strings allowing for * as a wildcard.
+ *
+ * @s1: Base string offering comparison template.
+ * @s2: String to be compared to template string.
+ *
+ * Return: 1 if strings are identical, 0 otherwise, -1 if null pointer entered.
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	/*Case null pointer(s)*/
+	if (!s1 || !s2)
+		return (-1);
+
+	/*Case Strings empty*/
+	if (!*s1 && !*s2)
+		return (1);
+
+	/*Case similar*/
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	/*Case wildcard*/
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+
+	/*Case dissimilar*/
+	return (0);
+}
