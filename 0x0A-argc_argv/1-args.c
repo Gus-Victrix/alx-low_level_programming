fbe3bd798def21196b0f53398b2dd0a731d24fb0
Gus-Victrix <iamfrsncis@gmail.com>
2023-04-08 04:04:00 +0300

    0x0A-argc_argv: MODIFIED TASK 1
    Now both argv and argc are declared as unused.

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,7 +9,7 @@
  * Return: Always 0.
  */
 
-int main(int argc, char **argv)
+int main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
 {
 	printf("%d\n", argc);
 	return (0);
