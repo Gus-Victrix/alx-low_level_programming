6bac2b1c188b8d7fcc9d52d286c20e8fd476a1d8
Gus-Victrix <iamfrsncis@gmail.com>
2023-04-08 06:47:24 +0300

    0x0A-argc_argv: MODIFIED TASK 4
    Now main() checks for non numerical strings and returns error if found.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,8 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+int isnum(char *s);
 /**
  * main - Adds positive numbers given as program arguments and prints out sum.
  *
@@ -16,13 +18,33 @@ int main(int argc, char **argv)
 
 	for (counter = 1; counter < argc; counter++)
 	{
-		if (atoi(argv[counter]) == 0)
+		if(!isnum(argv[counter]))
 		{
 			printf("Error\n");
-				return (1);
+			return (1);
 		}
 		result += atoi(argv[counter]);
 	}
+
 	printf("%d\n", result);
 	return (0);
+}
+
+/**
+ * isnum - Checks if string output only contains base ten digits.
+ *
+ * @s: This is a pointer to the string to be checked.
+ *
+ * Return: 1 if input is purely a number string, 0 otherwise.
+ */
+
+int isnum(char *s)
+{
+	while (*s)
+	{
+		if (!isdigit(*s))
+			return (0);
+		s++;
+	}
+	return (1);
 }
