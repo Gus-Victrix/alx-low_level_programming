e4301124147326548bdf6d59205ba2034c55ebe3
Gus-Victrix <iamfrsncis@gmail.com>
2023-04-08 03:40:19 +0300

    0x0A-argc_argv: UPDATE TO TASK 0
    Added comment documentation for main().

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - Prints it's own name then exits.
+ *
+ * @argc: Number of commandline arguments passed to program.
+ * @argv: Array of pointers to commandline arguments.
+ *
+ * Return: Always 0.
+ */
+
 int main(int argc, char **argv)
 {
 	printf("%s", *argv);
