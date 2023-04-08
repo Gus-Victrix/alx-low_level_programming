4ccec9c8d9f07f187fa9c53c359c652b71ed1c45
Gus-Victrix <iamfrsncis@gmail.com>
2023-04-08 07:16:56 +0300

    0x0A-argc_argv: ADDED TASK 5
    Defines main() and coin_changer() which together convert input amount to least number of cent coins to be given as change.

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int coin_changer(int amt);
+
+/**
+ * main - Prints minimum number of coins to make change for input amount.
+ *
+ * @argc: Number of commandline arguments.
+ * @argv: Array of commandline arguments.
+ *
+ * Return: 0 if successful, 1 if issues encountered.
+ */
+
+int main(int argc, char **argv)
+{
+	int num = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num = coin_changer(atoi(*argv));
+
+	printf("%d\n", num);
+
+	return (0);
+}
+
+/**
+ * coin_changer - Converts amount to least number of 25, 10, 5, 2, and 1 cents.
+ *
+ * @amt: Amount of money to be converted.
+ *
+ * Return: Minimum number of coins to be given as change.
+ */
+
+int coin_changer(int amt)
+{
+	int i = 0;
+
+	i += amt / 25;
+	amt %= 25;
+
+	i += amt / 10;
+	amt %= 10;
+
+	i += amt / 5;
+	amt %= 5;
+
+	i += amt / 2;
+	amt %= 2;
+
+	i += amt;
+	return (i);
+}
