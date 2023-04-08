fec60c6916a0279b91fec3a0b98ac16c5eb4aacb
Gus-Victrix <iamfrsncis@gmail.com>
2023-04-08 03:56:58 +0300

    0x0A-argc_argv: ADDED TASK 1
    Program that prints the number of commandline arguments.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the number of commmandline arguments followed by a newline.
+ *
+ * @argc: Number of commandline arguments passed to the program.
+ * @argv: Pointer to array of commandline arguments to the program.
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
