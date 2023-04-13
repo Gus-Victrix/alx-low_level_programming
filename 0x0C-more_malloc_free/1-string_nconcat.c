c3c0d67c8a7678ea1c9d4830c1b2c3899d0dfdd5
Gus-Victrix <iamfrsncis@gmail.com>
2023-04-13 23:51:09 +0300

    0x0C-more_malloc_free: ADDED TASK 1
    Defines string_nconcat() which concatenates 2 input strings and returns address of output string.

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,110 @@
+#include "main.h"
+#include <stdlib.h>
+
+int _strlen(char *s);
+/**
+ * string_nconcat - Concatenates n characters of s2 with s1 and then terminates
+ *					the string with null.
+ *
+ * @s1: First string.
+ * @s2: Second string from which n bytes are to be obtained.
+ * @n: Number of bytes of s2 to be added to end of s1.
+ *
+ * Description: If NULL is passed, treats it as if empty string passed.
+ *				If n >= length of s2, uses entire s2 string.
+ *
+ * Return: Address of memory containing the output string. NULL if failed.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str = 0;
+	unsigned int i = 0, j = 0;
+
+	if (!n)
+		return (0);
+	if (!s1 && !s2)
+	{
+		str = malloc(1);
+		*str = '\0';
+		return (str);
+	}
+	if (!s1)
+		goto PLAIN_S2;
+	if (!s2)
+		goto PLAIN_S1;
+	if (_strlen(s2) < n)
+	{
+		str = malloc(_strlen(s1) + _strlen(s2) + 1);
+		if (!str)
+			return (0);
+		while (*(s1 + j))
+		{
+			*(str + j) = *(s1 + j);
+			j++;
+		}
+		while (*(s2 + i))
+		{
+			*(str + j + i) = *(s2 + i);
+			i++;
+		}
+		*(str + j + i) = 0;
+		return (str);
+	}
+
+	if (_strlen(s2) >= n)
+	{
+		str = malloc(_strlen(s1) + n + 1);
+		if (!str)
+			return (0);
+		while (*(s1 + j))
+		{
+			*(str + j) = *(s1 + j);
+			j++;
+		}
+		while (j < n)
+		{
+			*(str + j + i) = *(s2 + i);
+			i++;
+		}
+		*(str + j + i) = 0;
+		return (str);
+
+PLAIN_S1:
+	str = malloc(_strlen(s1) + 1);
+	if (!str)
+		return (0);
+	while (*(s1 + i))
+	{
+		*(str + i) = *(s1 + i);
+		i++;
+	}
+	*(str + i) = 0;
+	return (str);
+PLAIN_S2:
+	str = malloc(n + 1);
+	if (!str)
+		return (0);
+	while (*(s2 + i) && i <= n)
+	{
+		*(str + i) = *(s2 + i);
+		i++;
+	}
+	*(str + i) = 0;
+	return (str);
+}
+
+/**
+ * _strlen - Finds the length of input string.
+ * @s: String whose length is to be returned.
+ * Return: Length of input string.
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != 0; i++)
+		continue;
+
+	return (i);
+}
