93c8daba616134a321288e3541deb2c3b7987565
Gus-Victrix <iamfrsncis@gmail.com>
2023-04-14 05:43:31 +0300

    0x0C-more_malloc_free: FINAL MODIFICATION TO TASKS
    The code for most functions has been re-written after a few pointers by a friend. Also, Task 5 submitted.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,125 +1,43 @@
 #include "main.h"
 #include <stdlib.h>
 
-int _strlen(char *s);
 /**
- * string_nconcat - Concatenates n characters of s2 with s1 and then terminates
- *					the string with null.
+ * string_nconcat - Concatenate 2 strings into a new one
  *
- * @s1: First string.
- * @s2: Second string from which n bytes are to be obtained.
- * @n: Number of bytes of s2 to be added to end of s1.
+ * @s1: String one
+ * @s2: Strign two
+ * @n: Size of interest in s2
  *
- * Description: If NULL is passed, treats it as if empty string passed.
- *				If n >= length of s2, uses entire s2 string.
- *
- * Return: Address of memory containing the output string. NULL if failed.
+ * Return: 0 if issues, pointer to new str else (success)
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str = 0;
-	long int i = 0, j = 0;
+	char *result;
+	unsigned int len1, len2, i;
 
-	/*Case: both strings null*/
-	if (!s1 && !s2)
+	if (s1 == 0)
+		len1 = 0;
+	else
 	{
-		str = malloc(sizeof(*str));
-		/*Returning empty string*/
-		*str = '\0';
-		return (str);
+		for (len1 = 0; *(s1 + len1); len1++)
+			;
 	}
-
-	/*Case: only string 1 is null*/
-	if (!s1)
+	if (s2 == 0)
+		len2 = 0;
+	else
 	{
-		str = malloc(sizeof(*str) * (n + 1));
-		if (!str)
-			return (0);
-		/*Copying string 2*/
-		while (*(s2 + i) && i < (int) n)
-		{
-			*(str + i) = *(s2 + i);
-			i++;
-		}
-		/*Adding null terminator to end of string*/
-		*(str + i) = 0;
-		return (str);
+		for (len2 = 0; *(s2 + len2); len2++)
+			;
 	}
-
-	/*Case: only string 2 is null*/
-	if (!s2)
-	{
-		str = malloc(sizeof(*str) * (_strlen(s1) + 1));
-		if (!str)
-			return (0);
-		/*Copying string 1*/
-		while (*(s1 + i))
-		{
-			*(str + i) = *(s1 + i);
-			i++;
-		}
-		/*Adding null terminator to end of string*/
-		*(str + i) = 0;
-		return (str);
-	}
-
-	/*Case: String 2 smaller than n characters*/
-	if (_strlen(s2) < (int) n)
-	{
-		str = malloc(sizeof(*str) * (_strlen(s1) + _strlen(s2) + 1));
-		/*Checking malloc() output*/
-		if (!str)
-			return (0);
-		/*Copying string 1*/
-		while (*(s1 + j))
-		{
-			*(str + j) = *(s1 + j);
-			j++;
-		}
-		/*Copying string 2*/
-		while (*(s2 + i))
-		{
-			*(str + j + i) = *(s2 + i);
-			i++;
-		}
-		/*Adding null termination to string*/
-		*(str + j + i) = 0;
-		return (str);
-	}
-
-	/*Case: String 2 is longer than n*/
-	str = malloc(sizeof(*str) * (_strlen(s1) + (int) n + 1));
-	if (!str)
+	if (n < len2)
+		len2 = n;
+	result = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (result == 0)
 		return (0);
-	/*Copying string 1*/
-	while (*(s1 + j))
-	{
-		*(str + j) = *(s1 + j);
-		j++;
-	}
-	/*Copying string 2*/
-	while (j < (int) n)
-	{
-		*(str + j + i) = *(s2 + i);
-		i++;
-	}
-	/*Adding null terminator*/
-	*(str + j + i) = 0;
-	return (str);
-}
-
-/**
- * _strlen - Finds the length of input string.
- * @s: String whose length is to be returned.
- * Return: Length of input string.
- */
-
-int _strlen(char *s)
-{
-	int i = 0;
-
-	for (i = 0; s[i] != 0; i++)
-		continue;
-
-	return (i);
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+	for (i = 0; i < len2; i++)
+		result[len1 + i] = s2[i];
+	*(result + len1 + len2) = '\0';
+	return (result);
 }
--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,40 +1,60 @@
 #include "main.h"
 #include <stdlib.h>
 
-/**
- * _realloc - Resizes a given memory block in the heap.
- *
- * @ptr: Pointer to input memory block.
- * @old_size: Size in bytes of the initial memory block pointed to by ptr.
- * @new_size: Size in bytes of the newly allocated memory block.
- *
- * Description: The function may be used as any of malloc, calloc or free. It
- *				does not initialize the unused memory of the block though.
- *
- * Return: Pointer to resized memory block, Null if new_size = 0 while ptr != 0
- */
+char *_memcpy(char *dest, char *src, unsigned int n);
 
+/**
+ * _realloc - reallocates a memory block using malloc and free
+ *
+ * @ptr: Value to reallocate
+ * @old_size: Old_size memory
+ * @new_size: New size of memory
+ *
+ * Return: 0 if probleme, the new pointer to the new allocated
+ * memory
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	int lesser;
-	void *new_block = 0;
+	void *array;
+
+	if (ptr == 0)
+		return (malloc(new_size));
 
 	if (new_size == old_size)
 		return (ptr);
-	if (!new_size && ptr)
-		free(ptr);
-	new_block = malloc(new_size);
-	if (!new_block)
-		return (0);
 
-	lesser = (int) (new_size > old_size ? old_size : new_size);
-	if (ptr && old_size)
+	if (new_size == 0)
 	{
-		for (; lesser >= 0; lesser--)
-			*((char *)new_block + lesser) = *((char *)ptr + lesser);
-
 		free(ptr);
+		return (0);
 	}
 
-	return (new_block);
+	array = malloc(new_size);
+	if (array == 0)
+		return (0);
+
+	_memcpy(array, ptr, old_size);
+	free(ptr);
+
+	return (array);
+}
+
+/**
+ * _memcpy - copies memory area.
+ *
+ * @dest: Destination sting
+ * @src: Source string to copies into dest
+ * @n: Number of byte to copie
+ *
+ * Return: Dest string.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
 }
