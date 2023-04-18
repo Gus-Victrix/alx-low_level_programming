014e9c0e6b9513c306abc7669dd8c7591ab38915
Gus-Victrix <iamfrsncis@gmail.com>
2023-04-18 04:33:32 +0300

    0x0E-structures_typedef: MODIFIED FOR TASK 0
    main.h has been moved into dog.h, which now defines type dog_t.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
+typedef struct dog dog_t;
+
 /**
  * struct dog - Defines a record for use in recording dog data.
  *
@@ -16,6 +18,15 @@ struct dog
 	char *owner;
 };
 
-typedef dog dog;
 
-#endif /*DOG_H*/
+int _putchar(char c);
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+endif /*DOG_H*/
