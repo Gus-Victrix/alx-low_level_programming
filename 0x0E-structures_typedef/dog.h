8c6025a09187429e260f9f470dac21e989de2985
Gus-Victrix <iamfrsncis@gmail.com>
2023-04-18 04:36:54 +0300

    0x0E-structures_typedef: MODIFICATION TO TASK 0
    Now the header is better structured

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,22 +4,20 @@
 typedef struct dog dog_t;
 
 /**
- * struct dog - Defines a record for use in recording dog data.
+ * struct dog - Describe a dog
  *
- * @name: This is the name of the dog.
- * @age: This is the age of the dog.
- * @owner: This is the name of the dog's owner.
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: No more info.
  */
-
 struct dog
 {
 	char *name;
-	float age;
+	double age;
 	char *owner;
 };
-
-
-int _putchar(char c);
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
@@ -29,4 +27,4 @@ dog_t *new_dog(char *name, float age, char *owner);
 
 void free_dog(dog_t *d);
 
-endif /*DOG_H*/
+#endif
