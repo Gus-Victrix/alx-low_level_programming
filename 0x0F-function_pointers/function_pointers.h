c9a3707e00a90d341e048f5fa9e9358255de0037
Gus-Victrix <iamfrsncis@gmail.com>
2023-04-20 00:39:09 +0300

    0x0F-function_pointers: ADDED HEADER FILE
    Contains the function declarations for most of the functions used in this project.

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,13 @@
+#include <aio.h>
+#ifndef FUNC_PTR_H
+#define FUNC_PTR_H
+
+int _putchar(char);
+
+void print_name(char *, void (*)(char *));
+
+void array_iterator(int *, size_t, void (*)(int));
+
+int int_index(int *, int, int (*)(int));
+
+#endif /*FUNC_PTR_H*/
