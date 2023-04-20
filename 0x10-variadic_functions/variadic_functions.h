4ed4ca0c879912299abc571c31d06d9149b7c3ab
Gus-Victrix <iamfrsncis@gmail.com>
2023-04-21 00:07:18 +0300

    0x10-variadic_functions: ADDED TASK 3.
    Defines print_all() which prints any arguments passed.

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,8 @@
 #ifndef VARARG_H
 #define VARARG_H
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int _putchar(char);
 
@@ -10,5 +13,17 @@ void print_numbers(const char *, const unsigned int, ...);
 void print_strings(const char *, const unsigned int, ...);
 
 void print_all(const char * const, ...);
+
+/**
+ * struct format_str - Format str
+ *
+ * @type: Type of the argu
+ * @f: Pointer to function to print argu
+ */
+typedef struct format_str
+{
+	char *type;
+	void (*f)(va_list);
+} format_str;
 
 #endif /*VARARG_H*/
