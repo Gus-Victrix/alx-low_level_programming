d9881dca8fd7f4286bd9e3d4b525efd9d74bdcdc
Gus-Victrix <iamfrsncis@gmail.com>
2023-04-20 04:48:18 +0300

    0x0F-function_pointers: UPDATE TO TASK 4
    Added check for negative byte input.

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -16,16 +16,16 @@
 int main(int argc, char *argv[])
 {
 	unsigned char *main_ptr;
-	size_t size, i;
+	int size, i;
 
-	size = atoi(argv[1]);
 	main_ptr = (unsigned char *)main;
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	if (atoi(*(argv + 1)) < 0)
+	size = atoi(argv[1]);
+	if (size < 0)
 	{
 		printf("Error\n");
 		exit(2);
