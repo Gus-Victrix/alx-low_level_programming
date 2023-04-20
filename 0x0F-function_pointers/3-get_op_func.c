cebe439a7059b19bb245172056a25b7448da0be5
Gus-Victrix <iamfrsncis@gmail.com>
2023-04-20 03:09:09 +0300

    0x0F-function_pointers: MODIFICATION TO TASK 3
    Added declaration for get_op_func() that gets the correct function pointer based on the operator being used in the function parameter.

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - Gets the correct operation function based on input.
+ *
+ * @s: Operator used in argument to program.
+ *
+ * Return: Pointer to correct function to be used for the arithmetic operation.
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	}
+	int i = 0;
+
+	while (ops[i].op)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			break;
+		i++;
+	}
+	return (ops[i].f);
+}
