4c8136bc06e98ad1a880bd5c0963fc348b81bdad
Gus-Victrix <iamfrsncis@gmail.com>
2023-04-21 01:00:18 +0300

    0x10-variadic_functions: REDONE TASK 0
    Now actually does what it's supposed to do ;)

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,28 +1,29 @@
 #include "variadic_functions.h"
-
-
 /**
- * sum_them_all - Sum all the parameters passed.
+ * sum_them_all - Sums up all integral parameters.
  *
- * @n: Number of parameters.
+ * @n: Number of input integers
  *
  * Return: Sum of input parameters.
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i, sum = 0;
+	int sum = 0;
+	unsigned int i;
+	va_list args;
 
-	va_list param;
+	va_start(args, n);
 
-	if (n == 0)
+	if (!n)
 		return (0);
 
-	va_start(param, n);
-
 	for (i = 0; i < n; i++)
-		sum += va_arg(param, int);
+	{
+		sum += va_arg(args, int);
+	}
 
-	va_end(param);
+	va_end(args);
+
 	return (sum);
 }
