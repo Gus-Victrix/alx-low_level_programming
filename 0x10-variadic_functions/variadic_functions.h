8bbe23ced89bfd57892ff7d0239c2dd16a7aaee0
Gus-Victrix <iamfrsncis@gmail.com>
2023-04-20 21:46:51 +0300

    0x10-variadic_functions: ADDED MAIN HEADER FILE FOR THE TASKS
    Contains prototypes of the defined functions.

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,14 @@
+#ifndef VARARG_H
+#define VARARG_H
+
+int _putchar(char);
+
+int sum_them_all(const unsigned int, ...);
+
+void print_numbers(const char *, const unsigned int, ...);
+
+void print_strings(const char *, const unsigned int, ...);
+
+void print_all(const char * const, ...);
+
+#endif /*VARARG_H*/
