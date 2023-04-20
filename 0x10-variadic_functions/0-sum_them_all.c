5a85ab67e9d14bd03b08c399858816f5c42b5596
Gus-Victrix <iamfrsncis@gmail.com>
2023-04-21 01:17:26 +0300

    0x10-variadic_functions: FINAL COMMIT TO THIS WORKING DIRECTORY...
    I really do not know what is wrong with the code at this point so am just winging it.

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,20 +12,20 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 	unsigned int i;
 
-	va_list args;
+	va_list param;
 
 
 	if (n == 0)
 		return (0);
 
-	va_start(args, n);
+	va_start(param, n);
 
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(args, int);
+		sum += va_arg(param, int);
 	}
 
-	va_end(args);
+	va_end(param);
 
 	return (sum);
 }
