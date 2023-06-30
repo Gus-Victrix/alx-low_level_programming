66c7a70c354307aedf7961a243ca89d223c7f668
Gus-Victrix <iamfrsncis@gmail.com>
2023-07-01 02:51:39 +0300

    0x12-singly_linked_lists: ADDED TASK 0
    File defines function that prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -9,18 +9,18 @@
 
 
 /**
- * struct list_s - singly linked list.
- * @str: string - malloced string.
- * @len: length of the string.
- * @next: points to the next node.
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
  *
- * Description: singly linked list node structure.
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct lists_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
 
@@ -34,4 +34,4 @@ list_t *add_node_end(list_t **head, const char *str);
 
 void free_list(list_t *head);
 
-endif /*LIST_H*/
+#endif /*LIST_H*/
