7c32b884a40a309576a3e2e3ef617dfa12d0e6a3
Gus-Victrix <iamfrsncis@gmail.com>
2023-07-01 05:09:41 +0300

    ADDED TASK 5
    File defines a constructor function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * print_message - Prints the message before main function is executed.
+ */
+void print_message(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back
+			!\n");
+}
