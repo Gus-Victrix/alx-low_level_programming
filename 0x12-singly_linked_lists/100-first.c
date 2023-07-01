2cc4288be495fb189f015d0df7540cf5675c4f55
Gus-Victrix <iamfrsncis@gmail.com>
2023-07-01 05:40:49 +0300

    MODIFICATION TO TASK 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,14 @@
 #include "lists.h"
 
+void print_message(void) __attribute__((constructor));
+
 /**
  * print_message - Prints the message before main function is executed.
  */
 void print_message(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back
-			!\n");
+	char *str1 = "You're beat! and yet, you must allow,\n";
+	char *str2 = "I bore my house upon my back!\n";
+
+	printf("%s%s", str1, str2);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -34,6 +34,4 @@ list_t *add_node_end(list_t **head, const char *str);
 
 void free_list(list_t *head);
 
-void print_message(void) __attribute__((constructor));
-
 #endif /*LIST_H*/
