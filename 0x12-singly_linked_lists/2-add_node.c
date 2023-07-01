cd310e8cf99e03140b1273aa68a8aaf369e62a3c
Gus-Victrix <iamfrsncis@gmail.com>
2023-07-01 05:02:30 +0300

    ADDED TASK 2
    File defines function that adds a node to a list_t list.

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+/**
+ * _strlen - Returns stringlength
+ *
+ * @str: Input string.
+ *
+ * Return: Length of input string.
+ */
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	if (str == NULL)
+		return (0);
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * add_node - Adds a node into a list_t list.
+ *
+ * @head: Head node of list_t list.
+ * @str: String to be added as a part of the node.
+ *
+ * Return: Null if errors. address of new node otherwise.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (str == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = _strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
