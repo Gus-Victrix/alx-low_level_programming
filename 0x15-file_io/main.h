f432a02ddf808c951c602a11b1f5b157f74a734d
Gus-Victrix <iamfrsncis@gmail.com>
2023-07-10 23:55:47 +0300

    MODIFICATIONS TO: main.h
    Renamed sizet to size_t

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,10 +5,11 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 
-ssize_t read_textfile(const char *filename, ssize_t letters);
+ssize_t read_textfile(const char *filename, size_t letters);
 
 int create_file(const char *filename, char *text_content);
 
