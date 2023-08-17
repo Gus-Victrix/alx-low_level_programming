9a65f179f479a5d287ed7d6c023223317c0c2f9c
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-17 10:43:55 +0300

    0x17-doubly_linked_lists: ADDED TASK 4
    Initial commit of the task

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list.
+ *
+ * @head: First member of the list.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+
+	if (head == NULL)
+		return;
+	while (current->next != NULL)
+	{
+		current = current->next;
+		free(current->prev);
+	}
+	free(current);
+}
