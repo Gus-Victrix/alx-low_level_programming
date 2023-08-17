fdc5fe410d6918ca818f47bb01c76f41a1d18821
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-17 14:18:44 +0300

    0x17-doubly_linked_lists: MODIFICATIONS TO TASK 8
    Simplified mechanism

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 
-size_t dlistint_len_(const dlistint_t *h);
 /**
  * delete_dnodeint_at_index - deletes specific node in a dlistint_t list.
  *
@@ -11,78 +10,33 @@ size_t dlistint_len_(const dlistint_t *h);
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	unsigned int len = 0, counter = 0;
+	unsigned int i = 0;
 	dlistint_t *current = NULL;
 
-	/* Case: empty list or NULL list */
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 		return (-1);
-
 	current = *head;
-	/* Case: popping first element */
-	if (index == 0)
-	{
-		*head = (*head)->next;
-		free(current);
-		return (1);
-	}
-	/* Case: out of bounds */
-	len = (unsigned int)dlistint_len_((const dlistint_t *)(*head));
-	if (index >= len)
-		return (-1);
-	/* Moving to required position */
-	while (counter < index)
+	while (current != NULL && i < index)
 	{
 		current = current->next;
-		counter++;
+		i++;
 	}
-	/* Isolating the index to be deleted */
-	current->prev->next = current->next;
-	if (current->next != NULL)
+	if (current == NULL)
+		return (-1);
+	if (current->next == NULL && i == 0)
+		*head = NULL;
+	else if (current->next != NULL && i == 0)
+	{
+		current->next->prev = NULL;
+		*head = current->next;
+	}
+	else if (i != 0 && current->next != NULL)
+	{
+		current->prev->next = current->next;
 		current->next->prev = current->prev;
+	}
+	else
+		current->prev->next = NULL;
 	free(current);
 	return (1);
-}
-
-
-/**
- * dlistint_len_ - returns the number of elements in a dlistint_t list.
- *
- * @h: Member of a dlistint_t list.
- *
- * Return: Number of elements in the list.
- */
-size_t dlistint_len_(const dlistint_t *h)
-{
-	dlistint_t *p = NULL;
-	int count = 0;
-
-	if (h == NULL)
-		return (0);
-
-	/* Counting the element stored in h */
-	count++;
-
-	/* Counting the elements before h */
-	if (h->prev != NULL)
-	{
-		p = h->prev;
-		while (p != NULL)
-		{
-			p = p->prev;
-			count++;
-		}
-	}
-
-	/* Counting the elements after h */
-	if (h->next != NULL)
-	{
-		p = h->next;
-		while (p != NULL)
-		{
-			p = p->next;
-			count++;
-		}
-	}
-	return (count);
 }
