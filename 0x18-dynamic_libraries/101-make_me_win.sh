d976dcaee3b72a5d516f3b043931fb0702c78047
Gus-Victrix <iamfrsncis@gmail.com>
2023-08-21 16:49:54 +0300

    0x18-dynamic_libraries: MODIFICATIONS TO TASK 3
    modified functions

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp -q "https://github.com/Gus-Victrix/alx-low_level_programming/raw/master/0/0x18-dynamic_libraries/libtest.so"
+wget -P /tmp -q "https://github.com/Gus-Victrix/alx-low_level_programming/raw/master/0/0x18-dynamic_libraries/libmask.so"
 export LD_PRELOAD="/tmp/libtest.so"
