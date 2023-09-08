575c06bcd290de4cb4c06d20746ee4144e8e15fd
Gus-Victrix <iamfrsncis@gmail.com>
2023-09-08 09:33:34 +0300

    UPDATED 3-hash_table_set.c to use the c standard library functions

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,65 +15,38 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node = NULL;
 	hash_node_t *current = NULL;
 
-	if (!key || !ht || !ht->array || !(ht->size) || !(*key) || !value)
+	if (!ht || !key || !*key || !ht->array || !ht->size || !value || !*value)
 		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = (char *)value;
+
+			if (current->value == NULL)
+				return (0);
+			return (1);
+		}
+		current = current->next;
+	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-	new_node->key = malloc(sizeof(char) * _strlen(key) + 1);
-	new_node->value = malloc(sizeof(char) * _strlen(value) + 1);
-	if (new_node->key == NULL || new_node->value == NULL)
+	new_node->value = strdup(value);
+	new_node->key = strdup(key);
+	if (!new_node->value || !new_node->key)
 	{
-		free(new_node->key);
 		free(new_node->value);
+		free(new_node->key);
 		free(new_node);
 		return (0);
 	}
-
-	_strcpy(new_node->key, key);
-	_strcpy(new_node->value, value);
-	index = key_index((const unsigned char *)key, ht->size);
-	current = ht->array[index];
-	if (current != NULL)
-		while (current->next != NULL)
-			current = current->next;
-	current = new_node;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 	return (1);
-}
-
-/**
- * _strcpy - Copies a string from src to dest including the null terminator.
- *
- * @dest: Destination address.
- * @src: Source of the string.
- *
- * Return: The destination
- */
-char *_strcpy(char *dest, const char *src)
-{
-	size_t i = 0;
-
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return dest;
-}
-
-/**
- * _strlen - Finds the length of a string.
- *
- * @s: String whose length is to be found.
- *
- * Return: Length of the string.
- */
-unsigned long _strlen(const char *s)
-{
-	unsigned long i = 0;
-
-	while (s[i])
-		i++;
-	return (i);
 }
