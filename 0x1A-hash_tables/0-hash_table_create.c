2a0e201cdd2885ea2b9fb51637a5d0bed8bd31e9
Gus-Victrix <iamfrsncis@gmail.com>
2023-09-08 06:14:55 +0300

    ADDED 0-hash_table_create.c

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a new hash table.
+ *
+ * @size: Size of the array.
+ *
+ * Return: pointer to the newly created hash table. NULL if failed.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table = NULL;
+
+	if (size == 0)
+		return (NULL);
+
+	new_table = malloc(sizeof(hash_table_t));
+
+	if (new_table == NULL)
+		return (NULL);
+
+	new_table->array = malloc(sizeof(hash_node_t) * size);
+
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+
+	return (new_table);
+}
