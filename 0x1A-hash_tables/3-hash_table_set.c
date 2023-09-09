dc20632895a000dfd35ea60e2f0eaf3477045b37
Gus-Victrix <iamfrsncis@gmail.com>
2023-09-09 05:45:53 +0300

    UPDATE to the set value function of the hash table.
    Now an empty string can be added.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node = NULL;
 	hash_node_t *current = NULL;
 
-	if (!ht || !key || !*key || !ht->array || !ht->size || !value || !*value)
+	if (!ht || !key || !*key || !ht->array || !ht->size || !value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
