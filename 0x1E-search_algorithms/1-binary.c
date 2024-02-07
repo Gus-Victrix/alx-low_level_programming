a89dbb2c1dde97d1ef35bb8418d20002379c0956
Gus-Victrix <iamfrsncis@gmail.com>
2024-02-07 21:42:59 +0300

    feat: binary_search now prints array before searching.

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,26 @@
 #include "search_algos.h"
 
 /**
+ * print_array - prints an array of integers
+ *
+ * @array: pointer to the first element of the array to print
+ * @size: number of elements in array
+ */
+void print_array(int *array, size_t size)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = 0; i < size; i++)
+	{
+		if (i == size - 1)
+			printf("%d\n", array[i]);
+		else
+			printf("%d, ", array[i]);
+	}
+}
+
+/**
  * binary_search - searches for a value in sorted int array using binary search
  *
  * @array: pointer to the first element of the array to search in
@@ -11,9 +31,8 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t left;  /* left bound */
-	size_t right; /* right bound */
-	size_t mid;  /* middle index */
+	size_t left, right; /* left and right bounds */
+	size_t mid; /* middle index */
 
 	if (!array) /* Edge case: array is NULL */
 		return (-1);
@@ -21,6 +40,7 @@ int binary_search(int *array, size_t size, int value)
 		return (-1);
 	if (size == 1) /* Edge case: size is 1 */
 	{
+		print_array(array, size);
 		if (array[0] == value)
 			return (0);
 		else
@@ -28,6 +48,7 @@ int binary_search(int *array, size_t size, int value)
 	}
 	if (size == 2) /* Edge case: size is 2 */
 	{
+		print_array(array, size);
 		if (array[0] == value)
 			return (0);
 		if (array[1] == value)
@@ -39,6 +60,7 @@ int binary_search(int *array, size_t size, int value)
 	while (left <= right)
 	{
 		mid = (left + right) / 2;
+		print_array(array + left, right - left + 1);
 		if (array[mid] < value)
 			left = mid + 1;
 		else if (array[mid] > value)
