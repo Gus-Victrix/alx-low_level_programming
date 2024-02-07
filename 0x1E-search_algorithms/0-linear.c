5c2402e8b4e5a37b113b76a1ff5e76f388d6c276
Gus-Victrix <iamfrsncis@gmail.com>
2024-02-07 03:40:26 +0300

    docs: Removed excessive documentation

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,49 +1,5 @@
 #include "search_algos.h"
 /**
- * +--------------------------------------------------------------------------+
- * |                        0x01E.C-SEARCH ALGORITHMS                         |
- * +--------------------------------------------------------------------------+
- * |   ALX Full Stack Software Engineering Program Project.                   |
- * +--------------------------------------------------------------------------+
- * |   Author: Francis Nderitu                                                |
- * |   Github: https://github.com/Gus-Victrix                                 |
- * |   LinkedIn: https://linkedin.com/in/francis-njoroge-nderitu              |
- * |   Date: 07th February, 2024                                              |
- * +--------------------------------------------------------------------------+
- * |   You may use this file for whatever purpose you wish.                   |
- * |   LICENCE MIT.                                                           |
- * +--------------------------------------------------------------------------+
- * |  The functions defined here are for use in the search algorithms project.|
- * +--------------------------------------------------------------------------+
- */
-
-/**
- * +-----------------------------------+
- * |      Linear Search Algorithm.     |
- * +-----------------------------------+
- * |  Time Complexity: O(n)            |
- * |  Space Complexity: O(1)           |
- * +-----------------------------------+
- * |  Procedure:                       |
- * +-----------------------------------+
- * |  1. Take the value to be checked. |
- * |  2. Take array of values to be    |
- * |     searched.                     |
- * |  3. Loop: While index < length of |
- * |     the array                     |
- * |  4.    Compare value against      |
- * |        array member at current    |
- * |        index.                     |
- * |  5.    If they match: Return the  |
- * |        current index of the array |
- * |  6.    Else: Increase the array   |
- * |        index by one.              |
- * |  7. Return -1 if value is not     |
- * |     found in the array.           |
- * +-----------------------------------+
- */
-
-/**
  * linear_search - Use linear search to find a value in an array.
  *
  * @array: Pointer to the first element of the array.
